fff8f15b53695873c308c7792fb991d00b013e58
Belhadj Ahmed Walid <belhadj.ahmedwalid1@gmail.com>
2024-07-07 22:10:23 +0100

    fixed provider problem for primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {HeaderComponent} from "./components/header/header.component";
-import {AboutComponent} from "./components/about/about.component";
-import {ProjectsComponent} from "./components/projects/projects.component";
-import {FooterComponent} from "./components/footer/footer.component";
-import {WorkExperienceComponent} from "./components/work-experience/work-experience.component";
-import {EducationComponent} from "./components/education/education.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { AboutComponent } from "./components/about/about.component";
+import { ProjectsComponent } from "./components/projects/projects.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { WorkExperienceComponent } from "./components/work-experience/work-experience.component";
+import { EducationComponent } from "./components/education/education.component";
+import { SkillsComponent } from "./components/skills/skills.component";
+import { TabViewModule } from 'primeng/tabview';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, AboutComponent, ProjectsComponent, FooterComponent, WorkExperienceComponent, EducationComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    AboutComponent,
+    ProjectsComponent,
+    FooterComponent,
+    WorkExperienceComponent,
+    EducationComponent,
+    SkillsComponent,
+    TabViewModule,
+    ToastModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,17 @@
-import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideHttpClient} from "@angular/common/http";
+import {MessageService} from "primeng/api";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    importProvidersFrom([BrowserAnimationsModule]),
+    MessageService,
   ]
 };
