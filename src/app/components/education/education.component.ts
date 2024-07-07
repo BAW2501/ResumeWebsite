682c05d6554f8b5958085b9c2efce5a7416b9852
Belhadj Ahmed Walid <belhadj.ahmedwalid1@gmail.com>
2024-07-07 22:11:03 +0100

    updated edu component

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -1,19 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ResumeService } from '../../services/resume.service';
-import {NgForOf} from "@angular/common";
+import { CarouselModule } from 'primeng/carousel';
+import { CardModule } from 'primeng/card';
+import { TagModule } from 'primeng/tag';
+import { ChipModule } from 'primeng/chip';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
   standalone: true,
   imports: [
+    CarouselModule,
+    CardModule,
+    TagModule,
+    ChipModule,
     NgForOf
   ],
   styleUrls: ['./education.component.css']
 })
-export class EducationComponent  {
+export class EducationComponent {
   education = this.resumeService.getResume().education;
 
   constructor(private resumeService: ResumeService) { }
-
 }
