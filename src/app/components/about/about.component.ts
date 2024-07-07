38b34f759da2e9262896fd7c1ce3781657961fe5
Belhadj Ahmed Walid <belhadj.ahmedwalid1@gmail.com>
2024-07-07 22:08:41 +0100

    updated about section

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { ResumeService } from '../../services/resume.service';
 import { CardModule } from 'primeng/card';
-import { ButtonModule } from 'primeng/button';
+import { ChipModule } from 'primeng/chip';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   standalone: true,
-  imports: [CardModule, ButtonModule],
+  imports: [CardModule, ChipModule],
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
